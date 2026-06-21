da72b9cb16dcba1774130443b97a024cada51228
Nitish Sharma <ernitishsharma361@gmail.com>
2026-06-22 00:25:52 +0530

    Refactor canvas setup and improve rendering performance with updated materials and group management

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+//size of canvas
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 // sence
 const scene = new THREE.Scene();
@@ -9,7 +14,7 @@ const scene = new THREE.Scene();
 //camera
 const camera = new THREE.PerspectiveCamera(
   75,
-  window.innerWidth / window.innerHeight,
+  sizes.width / sizes.height,
   0.1,
   1000
 );
@@ -21,34 +26,38 @@ texture.colorSpace = THREE.SRGBColorSpace;
 
 const top = new THREE.Mesh(
   new THREE.TorusGeometry( 0.8, 0.04, 16, 100 ),
-  new THREE.MeshBasicMaterial({ color: 0xc0c0c0 })
+  new THREE.MeshStandardMaterial({ color: 0xc0c0c0 })
 );
 top.rotation.x = Math.PI / 2;
 top.position.y = 1.84;
 
 const topbottom = new THREE.Mesh(
   new THREE.CylinderGeometry(0.8, 1, 0.3, 32),
-  new THREE.MeshBasicMaterial( { color: 0xE70708 } )
+  new THREE.MeshStandardMaterial( { color: 0xE70708 } )
 );
 
 topbottom.position.y = 1.65;
 
 const body = new THREE.Mesh(
   new THREE.CylinderGeometry(1, 1, 3, 32),
-  new THREE.MeshBasicMaterial({ map: texture })
+  new THREE.MeshStandardMaterial({ map: texture })
 );
 
 const base = new THREE.Mesh(
   new THREE.CylinderGeometry(1, 0.9, 0.18, 32),
-  new THREE.MeshBasicMaterial({ color: 0xc0c0c0 })
+  new THREE.MeshStandardMaterial({ color: 0xc0c0c0 })
 );
 
 base.position.y = -1.59;
 
-scene.add(base);
-scene.add(topbottom);
-scene.add(body);
-scene.add(top);
+const group = new THREE.Group();
+
+group.add(base);
+group.add(topbottom);
+group.add(body);
+group.add(top);
+
+scene.add(group);
 
 camera.position.z = 5;
 
@@ -57,17 +66,34 @@ const canvas = document.querySelector("canvas");
 
 //render
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 //controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+//resize
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth; //canvas width
+  sizes.height = window.innerHeight;  //canvas height
+  camera.aspect = sizes.width / sizes.height;     //camera aspect
+  camera.updateProjectionMatrix();                //camera update
+  renderer.setSize(sizes.width, sizes.height);     //renderer size
+});
+
+//light
+const light = new THREE.PointLight(0xffffff, 7);   //point light 
+light.position.set(0, 2, 5);  //light position
+scene.add(light);   
+
+//time
+const clock = new THREE.Clock(); //time
 
 //animate
 const animate = () => {
-  body.rotation.y += 0.01;
+  const delta = clock.getElapsedTime(); //time update by elapsed time
+  group.rotation.y = delta * 0.9;
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
